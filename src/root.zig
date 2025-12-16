51234fd0bfb41b616de4a81f18bb4a6f4f9c7f47
Gaiden-Spence <gaiden.anime@protonmail.com>
2025-12-15 23:29:04 -0500

    added custom allocator

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,49 @@
 const std = @import("std");
+const print = std.debug.print;
 
-pub fn add(a: i32, b: i32) i32 {
-    return a + b;
-}
+// Simple memory tracking allocator wrapper
+pub const TrackedAllocator = struct {
+    parent: std.mem.Allocator,
+    total_allocated: usize = 0,
 
-test "basic add" {
-    try std.testing.expectEqual(@as(i32, 4), add(2, 2));
-}
+    pub fn init(parent: std.mem.Allocator) TrackedAllocator {
+        return .{ .parent = parent };
+    }
+
+    pub fn allocator(self: *TrackedAllocator) std.mem.Allocator {
+        return .{
+            .ptr = self,
+            .vtable = &.{
+                .alloc = alloc,
+                .resize = resize,
+                .free = free,
+                .remap = remap,
+            },
+        };
+    }
+
+    fn alloc(ctx: *anyopaque, len: usize, ptr_align: std.mem.Alignment, ret_addr: usize) ?[*]u8 {
+        const self: *TrackedAllocator = @ptrCast(@alignCast(ctx));
+        self.total_allocated += len;
+        return self.parent.rawAlloc(len, ptr_align, ret_addr);
+    }
+
+    fn resize(ctx: *anyopaque, buf: []u8, buf_align: std.mem.Alignment, new_len: usize, ret_addr: usize) bool {
+        const self: *TrackedAllocator = @ptrCast(@alignCast(ctx));
+        return self.parent.rawResize(buf, buf_align, new_len, ret_addr);
+    }
+
+    fn free(ctx: *anyopaque, buf: []u8, buf_align: std.mem.Alignment, ret_addr: usize) void {
+        const self: *TrackedAllocator = @ptrCast(@alignCast(ctx));
+        self.parent.rawFree(buf, buf_align, ret_addr);
+    }
+
+    fn remap(ctx: *anyopaque, buf: []u8, buf_align: std.mem.Alignment, new_len: usize, ret_addr: usize) ?[*]u8 {
+        const self: *TrackedAllocator = @ptrCast(@alignCast(ctx));
+        return self.parent.rawRemap(buf, buf_align, new_len, ret_addr);
+    }
+
+    pub fn getStats(self: *TrackedAllocator) !void{
+        print("The total bytes allocated are {d}\n", .{self.total_allocated});
+    }
+};
