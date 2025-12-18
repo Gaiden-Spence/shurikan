022e032c2c5be49256de665c2bbf539249b224e9
Gaiden-Spence <gaiden.anime@protonmail.com>
2025-12-17 23:59:39 -0500

    added average byte allocation

--- src/root.zig
+++ src/root.zig
@@ -8,9 +8,11 @@ pub const TrackedAllocator = struct {
     total_bytes: usize = 0,
     current_bytes: usize = 0,
     bytes_freed: usize = 0,
+    peak_usage: usize = 0,
 
     total_allocations: usize = 0,
     total_deallocations: usize = 0,
+    active_allocations: usize = 0,
 
     pub fn init(parent: std.mem.Allocator) TrackedAllocator {
         return .{ .parent = parent };
@@ -33,7 +35,13 @@ pub const TrackedAllocator = struct {
 
         self.total_bytes += len;
         self.current_bytes += len;
-        self.allocations += 1;
+
+        if (self.current_bytes > self.peak_usage) {
+            self.peak_usage = self.current_bytes;
+        }
+
+        self.total_allocations += 1;
+        self.active_allocations += 1;
 
         return self.parent.rawAlloc(len, ptr_align, ret_addr);
     }
@@ -49,6 +57,9 @@ pub const TrackedAllocator = struct {
 
         self.bytes_freed += buf.len;
         self.current_bytes -= buf.len;
+
+        self.total_deallocations += 1;
+        self.active_allocations -= 1;
     }
 
     fn remap(ctx: *anyopaque, buf: []u8, buf_align: std.mem.Alignment, new_len: usize, ret_addr: usize) ?[*]u8 {
@@ -58,8 +69,13 @@ pub const TrackedAllocator = struct {
 
     pub fn printStats(self: *TrackedAllocator) !void {
 
-        print("The current usage of bytes are: {d}\n", .{self.current_bytes});
-        print("The total bytes allocated are: {d}\n", .{self.total_bytes});
-        print("The total bytes that have been freed are: {d}\n", .{self.bytes_freed});
+        const avg_allocation = @as(f64, @floatFromInt(self.total_bytes) / @floatFromInt(self.total_allocations));
+
+        print("The current usage of bytes are: {d}.\n", .{self.current_bytes});
+        print("The total bytes allocated are: {d}.\n", .{self.total_bytes});
+        print("The peak usage is {d}.\n", .{self.peak_usage});
+        print("The total bytes that have been freed are: {d}.\n", .{self.bytes_freed});
+        print("The total operations are: {d} allocs and {d} frees.\n", .{self.total_allocations});
+        print("The active allocations are: {d}.\n", .{self.active_allocations});
     }
 };
