323647cc9878fee0c17c945158802316ad1ab1ce
Gaiden-Spence <gaiden.anime@protonmail.com>
2026-01-04 19:34:34 -0500

    wrote test for getCurrentUsage

--- src/tests.zig
+++ src/tests.zig
@@ -1,3 +1,19 @@
-test {
-    _ = @import("root.zig");
+const std = @import("std");
+const testing = std.testing;
+const TrackedAllocator = @import("root.zig").TrackedAllocator;
+
+test "TrackedAllocator: test getCurrentUsage" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var tracked = TrackedAllocator.init(gpa.allocator());
+    defer tracked.memory_logs.deinit();
+
+    const allocator = tracked.allocator();
+
+    const bytes = try allocator.alloc(u8, 100);
+    defer allocator.free(bytes);
+
+    try testing.expectEqual(@as(usize, 100), tracked.current_bytes);
+
 }
