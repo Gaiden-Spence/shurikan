7860a4f946acf7cb9ffe27b8de9a7efe04952ff8
Gaiden-Spence <gaiden.anime@protonmail.com>
2026-01-06 22:34:49 -0500

    wrote test for bytes freed

--- src/tests.zig
+++ src/tests.zig
@@ -15,5 +15,19 @@ test "TrackedAllocator: test getCurrentUsage" {
     defer allocator.free(bytes);
 
     try testing.expectEqual(@as(usize, 100), tracked.current_bytes);
+}
 
+test "test getBytesFreed" {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+
+    var tracked = TrackedAllocator.init(gpa.allocator);
+    defer tracked.memory_logs.deinit();
+
+    const allocator = tracked.allocator();
+
+    const bytes = try allocator.alloc(u8, 100);
+    defer allocator.free(bytes);
+
+    try testing.expectEqual(@as(usize, 100), tracked.bytes_freed);
 }
