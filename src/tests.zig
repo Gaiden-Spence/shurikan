20cc73fd6936fbb46c524250759f22690b2a1537
Gaiden-Spence <gaiden.anime@protonmail.com>
2026-01-07 23:25:41 -0500

    added testing for arena allocations and fixed buffer allocations

--- src/tests.zig
+++ src/tests.zig
@@ -3,6 +3,7 @@ const testing = std.testing;
 const TrackedAllocator = @import("root.zig").TrackedAllocator;
 
 test "test getCurrentUsage" {
+    //Test GPA
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
 
@@ -22,6 +23,35 @@ test "test getCurrentUsage" {
 
     const present_bytes = tracked.getCurrentUsage();
 
+    //Test Arena Allocator
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    var tracked_arena = TrackedAllocator.init(arena.allocator());
+    const arena_allocator = tracked_arena.allocator();
+
+    const arena_bytes = try arena_allocator.alloc(u8, 100);
+    defer arena_allocator.free(arena_bytes);
+
+    const present_arena_bytes = tracked_arena.getCurrentUsage();
+
+    //Test Fixed Buffer Allocator
+    //Test FixedBufferAllocator
+    var buffer: [512]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+
+    var tracked_fba = TrackedAllocator.init(fba.allocator());
+    defer tracked_fba.memory_logs.deinit();
+
+    const fba_allocator = tracked_fba.allocator();
+
+    const fba_bytes = try fba_allocator.alloc(u8, 100);
+    defer fba_allocator.free(fba_bytes);
+
+    const present_fba_bytes = tracked_fba.getCurrentUsage();
+
+    try testing.expectEqual(@as(usize, 100), present_fba_bytes);
+    try testing.expectEqual(@as(usize, 100), present_arena_bytes);
     try testing.expectEqual(@as(usize, 11100), present_bytes);
 }
 
